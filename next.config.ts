c4ecedfaee9322f3e806637991962d8b37bb85bd
Aaron J. Lang <aaronjameslang@googlemail.com>
2026-05-19 00:34:23 +0100

    automate deploys

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/the-beauty-edit',
   images: {
     unoptimized: true,
     remotePatterns: [{ protocol: 'https', hostname: 'images.unsplash.com' }],
