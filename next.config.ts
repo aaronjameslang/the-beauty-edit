5b0b375759f55153eb6576674bd53200538aba81
Aaron J. Lang <aaronjameslang@googlemail.com>
2026-05-19 00:38:25 +0100

    harden security

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: '/the-beauty-edit',
+  reactStrictMode: true,
+  poweredByHeader: false,
   images: {
     unoptimized: true,
     remotePatterns: [{ protocol: 'https', hostname: 'images.unsplash.com' }],
